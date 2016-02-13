7895e770ff569a6e5d652ca80a03232874a1b317
admin <jgigault@student.42.fr>
2016-02-13 23:02:01 +0100

    tests

--- lib/planning_view.rb
+++ lib/planning_view.rb
@@ -5,3 +5,9 @@ require "planning_view/version"
 #ActiveSupport.on_load(:action_controller) do
 #  include PlanningView::Controller
 #end
+
+module PlanningView
+  class Engine < Rails::Engine
+    config.autoload_paths << File.expand_path("../app", __FILE__)
+  end
+end
