99e099b7fce86af40b89a74c7541bfd7afbcda1d
admin <jgigault@student.42.fr>
2016-02-16 00:24:03 +0100

    tests

--- lib/planning_view/controller.rb
+++ lib/planning_view/controller.rb
@@ -1,72 +1,76 @@
 require 'active_support/core_ext/string/output_safety'
 
 module PlanningView
+
   module Controller
-    #extend ActiveSupport::Concern
 
-    #def self.included(base)
-    #  base.send :extend, ClassMethods
-    #end
+    class NoCollectionError < StandardError; end
+    class EndDateBeforeOrEqualToStartDateError < StandardError; end
 
-    #module ClassMethods
-      def build_planning_view_variables(today: Date.today,
-                                        start_date:,
-                                        end_date:,
-                                        padding_time: 10,
-                                        default_zoom: 12,
-                                        group_by: :none,
-                                        timeslots: nil,
-                                        expandable: false,
-                                        highlight_name: nil,
-                                        data_attributes: {})
-        @planning_view_today = today
-        @planning_view_group_by = group_by.to_sym
-        @planning_view_timeslots = timeslots
-        @planning_view_expandable = expandable
-        @planning_view_highlight_name = highlight_name
-        @planning_view_data_attributes = data_attributes
-        @planning_view_default_zoom = default_zoom
+    def build_planning_view_variables(start_date: Date.today.beginning_of_year,
+                                        end_date: Date.today.end_of_year,
+                                       timeslots: [],
+                                     margin_time: {before: 10.years, after: 10.years},
+                                          marker: {date: Date.today, label: I18n.t('concepts.today')},
+                                    default_zoom: 12,
+                                        group_by: nil,
+                                  highlight_name: nil,
+                                      expandable: false,
+                             row_data_attributes: {})
 
-        @planning_view_highlight_start_date = start_date
-        @planning_view_highlight_end_date = end_date
-        @planning_view_highlight_nb_of_days = end_date - start_date + 1
+      raise NoCollectionError if @collection == nil
+      raise EndDateBeforeOrEqualToStartDateError if end_date <= start_date
 
-        @planning_view_start_date = start_date - padding_time.years
-        @planning_view_end_date = end_date + padding_time.years
-        @planning_view_nb_of_days = @planning_view_end_date - @planning_view_start_date + 1
+      @planning_view_marker_date = marker[:date]
+      @planning_view_marker_label = marker[:label]
+      @planning_view_group_by = group_by
+      @planning_view_timeslots = timeslots
+      @planning_view_expandable = expandable
+      @planning_view_highlight_name = highlight_name
+      @planning_view_row_data_attributes = row_data_attributes
+      @planning_view_default_zoom = default_zoom
 
-        @planning_view_highlight_width = (@planning_view_highlight_nb_of_days * 100 / @planning_view_nb_of_days).to_f
-        @planning_view_highlight_x = ((start_date - @planning_view_start_date + 1) * 100 / @planning_view_nb_of_days).to_f
-        @planning_view_today_x = ((@planning_view_today - @planning_view_start_date + 1) * 100 / @planning_view_nb_of_days).to_f
+      @planning_view_start_date = start_date
+      @planning_view_end_date = end_date
+      @planning_view_nb_of_days = end_date - start_date + 1
 
-        @planning_view_months = []
+      @planning_view_margins_start_date = start_date - margin_time[:before]
+      @planning_view_margins_end_date = end_date + margin_time[:after]
+      @planning_view_margins_nb_of_days = @planning_view_margins_end_date - @planning_view_margins_start_date + 1
 
-        current_year = 0
-        month = @planning_view_start_date.beginning_of_month
-        while month < @planning_view_end_date do
-          start_month_x = ((month - @planning_view_start_date + 1) * 100 / @planning_view_nb_of_days).to_f
+      @planning_view_width = (@planning_view_nb_of_days * 100 / @planning_view_margins_nb_of_days).to_f
+      @planning_view_x = ((start_date - @planning_view_margins_start_date + 1) * 100 / @planning_view_margins_nb_of_days).to_f
+      @planning_view_marker_x = ((@planning_view_marker_date - @planning_view_margins_start_date + 1) * 100 / @planning_view_margins_nb_of_days).to_f
 
-          @planning_view_months << {
-            date: month,
-            name: "#{'<span>' if current_year != month.year}#{I18n.t("date.abbr_month_names")[month.month]}#{"<br /></span><span>#{month.year}</span>".html_safe if current_year != month.year}",
-            x: start_month_x,
-            width: (((month + 1.month) - month) * 100 / @planning_view_nb_of_days).to_f,
-            highlight: (month >= start_date && month < end_date)
-          }
+      @planning_view_months = []
 
-          current_year = month.year if current_year != month.year
-          month = month + 1.month
-        end
+      current_year = 0
+      month = @planning_view_start_date.beginning_of_month
+      while month < @planning_view_end_date do
+        start_month_x = ((month - @planning_view_margins_start_date + 1) * 100 / @planning_view_margins_nb_of_days).to_f
 
-        nb_of_years = padding_time * 2 + 1
-        @planning_view_available_zoom = {
-          60 => (100.0 / 66  * nb_of_years * 12).to_i,
-          24 => (100.0 / 29  * nb_of_years * 12).to_i,
-          12 => (100.0 / 15  * nb_of_years * 12).to_i,
-          6  => (100.0 / 7   * nb_of_years * 12).to_i,
-          3  => (100.0 / 3.5 * nb_of_years * 12).to_i
+        @planning_view_months << {
+          date: month,
+          name: "#{'<span>' if current_year != month.year}#{I18n.t("date.abbr_month_names")[month.month]}#{"<br /></span><span>#{month.year}</span>".html_safe if current_year != month.year}",
+          x: start_month_x,
+          width: (((month + 1.month) - month) * 100 / @planning_view_margins_nb_of_days).to_f,
+          highlight: (month >= start_date && month < end_date)
         }
+
+        current_year = month.year if current_year != month.year
+        month = month + 1.month
       end
-    #end
+
+      nb_of_years = ((@planning_view_margins_end_date - @planning_view_margins_start_date + 1) / 365).to_f
+      @planning_view_available_zoom = {
+        60 => (100.0 / 66  * nb_of_years * 12).to_i,
+        24 => (100.0 / 29  * nb_of_years * 12).to_i,
+        12 => (100.0 / 15  * nb_of_years * 12).to_i,
+        6  => (100.0 / 7   * nb_of_years * 12).to_i,
+        3  => (100.0 / 3.5 * nb_of_years * 12).to_i
+      }
+    end
+
   end
+
 end
--- spec/planning_view/controller_spec.rb
+++ spec/planning_view/controller_spec.rb
@@ -1,28 +1,65 @@
 require 'spec_helper'
 
 class MockController #< ActionController::TestCase
+  attr_accessor :collection
   include PlanningView::Controller
 end
 
 describe 'controller' do
-  it 'works' do
-    cat = Category.create
-    p = Project.create category: cat, negotiation_start: Date.new(2015), negotiation_end: Date.new(2016)
-    c = MockController.new
-    @collection = Project.all
 
-    start_date = Date.new(2015)
-    end_date = Date.new(2015, 12, 31)
-    c.build_planning_view_variables(start_date: start_date,
-                                    end_date: end_date,
-                                    timeslots: {start_attribute: :negotiation_start,
-                                                end_attribute: :negotiation_end})
+  let(:start_date_2015) {Date.new(2015, 1, 1)}
+  let(:end_date_2015)   {Date.new(2015, 12, 31)}
+  let(:my_controller)   {MockController.new}
 
-    expect(c.instance_variable_get('@planning_view_today')).to eq(Date.today)
-    expect(c.instance_variable_get('@planning_view_highlight_start_date')).to eq(start_date)
-    expect(c.instance_variable_get('@planning_view_highlight_end_date')).to eq(end_date)
-    expect(c.instance_variable_get('@planning_view_highlight_nb_of_days')).to eq(365)
-    expect(c.instance_variable_get('@planning_view_highlight_name')).to eq(nil)
-    expect(c.instance_variable_get('@planning_view_group_by')).to eq(:none)
+  context 'build_planning_view_variables' do
+
+    context 'variables' do
+      before :each do
+        my_controller.collection = []
+      end
+
+      it 'sets default values' do
+        my_controller.build_planning_view_variables
+
+        default_start_date = Date.today.beginning_of_year
+        default_end_date = Date.today.end_of_year
+
+        expect(my_controller.instance_variable_get('@planning_view_start_date')).to eq(default_start_date)
+        expect(my_controller.instance_variable_get('@planning_view_end_date')).to eq(default_end_date)
+        expect(my_controller.instance_variable_get('@planning_view_nb_of_days')).to eq(default_end_date - default_start_date + 1)
+
+        expect(my_controller.instance_variable_get('@planning_view_margins_start_date')).to eq(default_start_date - 10.years)
+        expect(my_controller.instance_variable_get('@planning_view_margins_end_date')).to eq(default_end_date + 10.years)
+        expect(my_controller.instance_variable_get('@planning_view_margins_nb_of_days')).to eq((default_end_date + 10.years) - (default_start_date - 10.years) + 1)
+
+        expect(my_controller.instance_variable_get('@planning_view_name')).to eq(nil)
+        expect(my_controller.instance_variable_get('@planning_view_group_by')).to eq(nil)
+        expect(my_controller.instance_variable_get('@planning_view_timeslots')).to eq([])
+        expect(my_controller.instance_variable_get('@planning_view_expandable')).to eq(false)
+
+        expect(my_controller.instance_variable_get('@planning_view_marker_date')).to eq(Date.today)
+        expect(my_controller.instance_variable_get('@planning_view_marker_label')).to eq(I18n.t('concepts.today'))
+
+        expect(my_controller.instance_variable_get('@planning_view_row_data_attributes')).to eq({})
+      end
+    end
+
+    context "" do
+      before :each do
+        cat = Category.create
+        p = Project.create category: cat, negotiation_start: Date.new(2015), negotiation_end: Date.new(2016)
+        @collection = Project.all
+      end
+    end
+
+    context "validation" do
+      it "needs @collection" do
+        expect {
+          my_controller.build_planning_view_variables
+        }.to raise_error PlanningView::Controller::NoCollectionError
+      end
+    end
+
   end
+
 end
